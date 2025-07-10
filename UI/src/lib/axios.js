239c581e38ae495d1095670f419f7c142aae5bb8
Ashish <goyalraj78900@gmail.com>
2025-07-10 23:24:15 +0530

    deploy

--- UI/src/lib/axios.js
+++ UI/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const backendUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: backendUrl,
   withCredentials: true,
 });

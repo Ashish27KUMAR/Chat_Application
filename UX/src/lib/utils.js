cbb54bb7943a68338232de46811b7745b72a821d
Ashish <goyalraj78900@gmail.com>
2025-07-12 16:47:21 +0530

    fixed the problem

--- UX/src/lib/utils.js
+++ UX/src/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // true in production (https)
-    sameSite: "None", // ✅ allow cross-origin (Vercel ↔ Render)
+    secure: process.env.NODE_ENV === "production", // true only in production (https)
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // ✅
   });
 
   return token;
